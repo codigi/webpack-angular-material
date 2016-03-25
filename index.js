5ea7cc136e9469eb56d7a8824b85f4a7a71fe94e
Atanas Palavrov <palavrov@gmail.com>
2016-03-25 10:17:03 +0100

    Add implementation.

---
+++ index.js
@@ -0,0 +1,8 @@
+var jquery = require("jquery");
+var angular = require("angular");
+var angularAria = require("angular-aria");
+var angularAnimate = require("angular-animate");
+var angularMaterial = require("angular-material");
+var angularMaterialCss = require("style!css!angular-material/angular-material.css");
+
+module.exports = angularMaterial;
